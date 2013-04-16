757d2ffbdd16facf72e217805a0c728f99cb3776
tuliang <info@tuliang.org>
2013-04-16 18:04:36 +0800

    add vendor

--- lib/smart_tag.rb
+++ lib/smart_tag.rb
@@ -4,4 +4,14 @@ module SmartTag
   def self.create_tag(name, value)
     puts "#{name}  #{value}" 
   end
+
+  def self.root_path
+    @root_path ||= Pathname.new(File.dirname(File.expand_path('../', __FILE__)))
+  end
+  
+  def self.assets
+    Dir[root_path.join('vendor/assets/javascripts/smart_tag/**', '*.{js,css}')].inject([]) do |assets, path|
+      assets << Pathname.new(path).relative_path_from(root_path.join('vendor/assets/javascripts'))
+    end
+  end
 end
