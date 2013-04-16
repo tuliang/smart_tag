a725db258feae9d2a3f46c9c8e597f826cced0ef
tuliang <info@tuliang.org>
2013-04-16 20:31:05 +0800

    up

--- lib/smart_tag.rb
+++ lib/smart_tag.rb
@@ -4,10 +4,6 @@ require "smart_tag/version"
 
 module SmartTag
   def self.create_tag(name, value)
-    root_path ||= Pathname.new(File.dirname(File.expand_path('../', __FILE__)))
-    Dir[root_path.join('vendor/assets/javascripts/smart_tag/**', '*.{js,css}')].inject([]) do |assets, path|
-      puts assets << Pathname.new(path).relative_path_from(root_path.join('vendor/assets/javascripts'))
-    end
     puts "#{name}  #{value}" 
   end
 end
