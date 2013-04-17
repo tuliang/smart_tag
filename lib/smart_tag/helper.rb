af2d13834aeda17451c0d985f11d6a8fb5400b2c
tuliang <info@tuliang.org>
2013-04-17 12:17:31 +0800

    up

--- lib/smart_tag/helper.rb
+++ lib/smart_tag/helper.rb
@@ -1,14 +1,14 @@
 module SmartTag
   module Helper
     
-    def tadding_tag(name, content = nil, options = {})
-      id = sanitize_to_id(name)
-      input_html = { :id => id }.merge(options.delete(:input_html) || {})
-      output = ActiveSupport::SafeBuffer.new
-      output << text_area_tag(name, content, input_html)
-    end
+    # def tadding_tag(name, content = nil, options = {})
+    #   id = sanitize_to_id(name)
+    #   input_html = { :id => id }.merge(options.delete(:input_html) || {})
+    #   output = ActiveSupport::SafeBuffer.new
+    #   output << text_area_tag(name, content, input_html)
+    # end
     
-    def tadding(name, method, options = {})
+    def smart_tag(name, method, options = {})
       input_html = (options.delete(:input_html) || {})
       hash = input_html.stringify_keys
       instance_tag = ActionView::Base::InstanceTag.new(name, method, self, options.delete(:object))
@@ -20,8 +20,8 @@ module SmartTag
   end
   
   module Builder
-    def tadding(method, options = {})
-      @template.send("tadding", @object_name, method, objectify_options(options))
+    def smart_tag(method, options = {})
+      @template.send("smart_tag", @object_name, method, objectify_options(options))
     end
   end
 end
