4e4362745aa1bab3dd768ca4703e36f60a0f3e12
tuliang <info@tuliang.org>
2013-04-17 17:42:43 +0800

    up

--- lib/smart_tag/helper.rb
+++ lib/smart_tag/helper.rb
@@ -11,10 +11,10 @@ module SmartTag
     def smart_tag(name, method, options = {})
       input_html = (options.delete(:input_html) || {})
       hash = input_html.stringify_keys
-      instance_tag = ActionView::Base::InstanceTag.new(name, method, self, options.delete(:object))
+      instance_tag = ActionView::Base::FormBuilder.new(name, method, self, options.delete(:object))
       instance_tag.send(:add_default_name_and_id, hash)      
       output_buffer = ActiveSupport::SafeBuffer.new
-      output_buffer << instance_tag.to_text_area_tag(input_html)
+      output_buffer << '<ul class="smart_tag"></ul>'#instance_tag.to_text_area_tag(input_html)
     end
      
   end
