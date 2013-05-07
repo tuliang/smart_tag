71b5a5fe9ed9cee7256a242f786086a6bc5ef7a4
tuliang <info@tuliang.org>
2013-05-07 19:54:48 +0800

    up

--- lib/smart_tag/helper.rb
+++ lib/smart_tag/helper.rb
@@ -8,20 +8,22 @@ module SmartTag
     #   output << text_area_tag(name, content, input_html)
     # end
     
-    def smart_tag(name, method, options = {})
-      input_html = (options.delete(:input_html) || {})
-      hash = input_html.stringify_keys
-      instance_tag = ActionView::Base::FormBuilder.new(name, method, self, options.delete(:object))
-      instance_tag.send(:add_default_name_and_id, hash)      
-      output_buffer = ActiveSupport::SafeBuffer.new
-      output_buffer << instance_tag.to_text_area_tag(input_html)
+    def smart_tag(name)
+      name
+      # input_html = (options.delete(:input_html) || {})
+      # hash = input_html.stringify_keys
+      # instance_tag = ActionView::Base::FormBuilder.new(name, method, self, options.delete(:object))
+      # instance_tag.send(:add_default_name_and_id, hash)      
+      # output_buffer = ActiveSupport::SafeBuffer.new
+      # output_buffer << instance_tag.to_text_area_tag(input_html)
+
     end
      
   end
   
-  module Builder
-    def smart_tag(method, options = {})
-      @template.send("smart_tag", @object_name, method, objectify_options(options))
-    end
-  end
+  # module Builder
+  #   def smart_tag(method, options = {})
+  #     @template.send("smart_tag", @object_name, method, objectify_options(options))
+  #   end
+  # end
 end
