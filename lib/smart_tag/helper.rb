8a4b3317482707496c396511b2ccaffe37fcba60
tuliang <info@tuliang.org>
2013-05-07 20:08:54 +0800

    up

--- lib/smart_tag/helper.rb
+++ lib/smart_tag/helper.rb
@@ -1,5 +1,5 @@
 module SmartTag
-  # module Helper
+  module Helper
     
     # def tadding_tag(name, content = nil, options = {})
     #   id = sanitize_to_id(name)
@@ -8,22 +8,20 @@ module SmartTag
     #   output << text_area_tag(name, content, input_html)
     # end
     
-  def smart_tag(f, method)
-    f.collection_select :video_tag_ids, VideoTag.order(:title), :id, :title,{},{ :multiple => true, :size => 25, :style => "width:500px;", :class => 'smart_tag', :controller => 'video_tag', :method => method }
-    # input_html = (options.delete(:input_html) || {})
-    # hash = input_html.stringify_keys
-    # instance_tag = ActionView::Base::FormBuilder.new(name, method, self, options.delete(:object))
-    # instance_tag.send(:add_default_name_and_id, hash)      
-    # output_buffer = ActiveSupport::SafeBuffer.new
-    # output_buffer << instance_tag.to_text_area_tag(input_html)
-
-  end
+    def smart_tag(name, method, options = {})
+      input_html = (options.delete(:input_html) || {})
+      hash = input_html.stringify_keys
+      instance_tag = ActionView::Base::FormBuilder.new(name, method, self, options.delete(:object))
+      instance_tag.send(:add_default_name_and_id, hash)      
+      output_buffer = ActiveSupport::SafeBuffer.new
+      output_buffer << instance_tag.to_text_area_tag(input_html)
+    end
      
-  # end
+  end
   
-  # module Builder
-  #   def smart_tag(method, options = {})
-  #     @template.send("smart_tag", @object_name, method, objectify_options(options))
-  #   end
-  # end
+  module Builder
+    def smart_tag(method, options = {})
+      @template.send("smart_tag", @object_name, method, objectify_options(options))
+    end
+  end
 end
--- lib/smart_tag/version.rb
+++ lib/smart_tag/version.rb
@@ -1,3 +1,3 @@
 module SmartTag
-  VERSION = "0.4.3"
+  VERSION = "0.4.4"
 end
