90f9eaf3f681a39d830237e5084becf65515db16
tuliang <info@tuliang.org>
2013-05-07 17:23:51 +0800

    up

--- vendor/assets/javascripts/smart_tag/smart_tag.js
+++ vendor/assets/javascripts/smart_tag/smart_tag.js
@@ -17,7 +17,7 @@
 
       that.clearText = function(text) {
         // \s 匹配任何空白字符，包括空格、制表符、换页符等等。等价于[ \f\n\r\t\v]。
-        return text.replace(/^\s+|\s+$/g, ""); // 去掉开头或结尾的任何空白字符
+        return text.replace(/^\s+|\s+$/g, ""); // 去掉开头和结尾的任何空白字符
       };
 
       that.getOptions = function(selector) {
